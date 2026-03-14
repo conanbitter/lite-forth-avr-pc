416b7a27e25d4675b119f6eedd540fd3dd41be5e
Conan Bitter <conan.bitter@gmail.com>
2026-03-14 19:18:34 +0300

    Add some definitions

--- include/include.h
+++ include/include.h
@@ -1,4 +1,24 @@
 #ifndef INCLUDE_H
 #define INCLUDE_H
 
+#include <stdint.h>
+
+#define INPUT_MAXLENGTH (80)
+#define WORD_MAXLENGTH (32)
+
+typedef uint8_t fchar;
+
+typedef int16_t fcell;
+typedef uint16_t fucell;
+
+typedef struct WordHead
+{
+    struct WordHead* link;
+    fchar flags;
+    fchar len;
+    char name[WORD_MAXLENGTH];
+    fucell cfa;
+} WordHead;
+
+
 #endif
